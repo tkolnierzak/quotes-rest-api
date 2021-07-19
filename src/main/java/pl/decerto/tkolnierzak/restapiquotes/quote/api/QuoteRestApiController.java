914eddfa174b07a8e2e1bac5979b8636accac047
Kołnierzak Tomasz <tomasz.kolnierzak@comp.com.pl>
2021-07-19 17:09:41 +0200

    Create Rest Controller

--- src/main/java/pl/decerto/tkolnierzak/restapiquotes/quote/api/QuoteRestApiController.java
+++ src/main/java/pl/decerto/tkolnierzak/restapiquotes/quote/api/QuoteRestApiController.java
@@ -2,8 +2,14 @@ package pl.decerto.tkolnierzak.restapiquotes.quote.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.decerto.tkolnierzak.restapiquotes.quote.service.QuoteService;
 
 @RestController
 @RequestMapping
 public class QuoteRestApiController {
+    private QuoteService quoteService;
+
+    public QuoteRestApiController(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
 }
