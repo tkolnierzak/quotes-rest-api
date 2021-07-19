3876607252025639e03864e6f1ab4ba28f31e021
Kołnierzak Tomasz <tomasz.kolnierzak@comp.com.pl>
2021-07-19 16:35:26 +0200

    Initial commit

---
+++ src/main/java/pl/decerto/tkolnierzak/restapiquotes/RestApiQuotesApplication.java
@@ -0,0 +1,13 @@
+package pl.decerto.tkolnierzak.restapiquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/decerto/tkolnierzak/restapiquotes/RestApiQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.decerto.tkolnierzak.restapiquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
